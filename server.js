43a05d803292a427c44b107b793a9667f86f1145
2316722 <206804375+2316722@users.noreply.github.com>
2025-11-07 12:02:14 +0900

    get

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 // 今回はクライアントサイドもルーティングもないので何もimportしない
 const kv = await Deno.openKv();
 
-await kv.set(['妖精', 'エルフィン'], { color: green, lank: 3 });
+await kv.set(['妖精', 'エルフィン'], { color: 'green', lank: 3 });
+await kv.set(['妖精', 'マヨ'], { color: 'red', lank: 3 });
+await kv.set(['妖精', 'マリー'], { color: 'yellow', lank: 2 });
+
+const a = await kv.get(['妖精', 'エルフィン']);
+console.log(a);
