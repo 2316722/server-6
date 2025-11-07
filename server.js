641da4b19afd4be35affe6b9a11264d2ce0a8ab0
2316722 <206804375+2316722@users.noreply.github.com>
2025-11-07 12:10:42 +0900

    delete

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 // 今回はクライアントサイドもルーティングもないので何もimportしない
 const kv = await Deno.openKv();
 
+//set
 await kv.set(['妖精', 'エルフィン'], { color: 'green', lank: 3 });
 await kv.set(['妖精', 'マヨ'], { color: 'red', lank: 3 });
 await kv.set(['妖精', 'マリー'], { color: 'yellow', lank: 2 });
 
+//get
 const get = await kv.get(['妖精', 'エルフィン']);
 console.log(get);
 
+//list
 const list = await kv.list({ prefix: ['妖精'] });
-
 for await (const e of list) {
-  console.log(e.key[1]);
-  console.log(e.value.color);
+  console.log(e.key[1] + ':' + e.value.color);
+}
+
+//delete
+await kv.delete(['妖精', 'マリー']);
+const list2 = await kv.list({ prefix: ['妖精'] });
+for await (const e of list2) {
+  console.log(e.key[1] + ':' + e.value.color);
 }
