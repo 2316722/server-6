55bf2dd9583679d178a0a34578539198348d8838
2316722 <206804375+2316722@users.noreply.github.com>
2025-11-07 12:06:10 +0900

    list

--- server.js
+++ server.js
@@ -5,5 +5,12 @@ await kv.set(['妖精', 'エルフィン'], { color: 'green', lank: 3 });
 await kv.set(['妖精', 'マヨ'], { color: 'red', lank: 3 });
 await kv.set(['妖精', 'マリー'], { color: 'yellow', lank: 2 });
 
-const a = await kv.get(['妖精', 'エルフィン']);
-console.log(a);
+const get = await kv.get(['妖精', 'エルフィン']);
+console.log(get);
+
+const list = await kv.list({ prefix: ['妖精'] });
+
+for await (const e of list) {
+  console.log(e.key);
+  console.log(e.value);
+}
